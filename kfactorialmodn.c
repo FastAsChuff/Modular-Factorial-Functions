ac2363fa9f5b70a6907a9606d21d72e1b1ca9fcf
Simon Goater <124365713+FastAsChuff@users.noreply.github.com>
2025-12-16 12:25:23 +0000

    Add files via upload

--- kfactorialmodn.c
+++ kfactorialmodn.c
@@ -4,11 +4,13 @@
 // Author: Simon Goater Dec 2025
 // -----------------------------
 // Simple functions to calculate k! mod n.
-// getkfactmodn1() - k,n < 2^64 (Slowest)
-// getkfactmodn6() - n < 2^64, k < 2^32, n must be odd and array primes must contain all primes <= k in ascending order.(Fastest)
-// getkfactmodn3() - k,n < 2^64, n must be odd. Define macro KFACTORIALMODUSEMG to use this function.
-//    Requires https://github.com/FastAsChuff/Fast-Modular-Exponentiation/blob/main/modpowu64.c
+// getkfactmodn1() - k,n < 2^64 (Slowest on x64)
+// Define macro KFACTORIALMODUSEMG to use the following functions.
+//    Require https://github.com/FastAsChuff/Fast-Modular-Exponentiation/blob/main/modpowu64.c
 //    and https://github.com/FastAsChuff/Fast-Modular-Inverse-Modulo-Powers-Of-2/blob/main/fastmodinvpow2fns.c
+// getkfactmodn6() - n < 2^64, k < 2^32, n must be odd and pre-computed array primes must contain all primes <= k in ascending 
+//    order. (Fastest on x64)
+// getkfactmodn3() - k,n < 2^64, n must be odd. 
 // COPYRIGHT: Software is given as is without guarantee or warranty. It is offered free to use, modify, copy, or distribute
 // with conspicuous attribution for any purpose.
 //===============================================================================================================================
@@ -39,19 +41,6 @@ uint64_t getkfactmodn3(uint64_t k, uint64_t n) {
   uint64_t f2 = fromu64mg(fr2, n, ninv, twoto64modn);
   if (k & 0x1u) f1 = ((U128)k*f1) % n;
   return ((U128)f2*f1) % n;
-}
-#endif
-
-uint64_t getkfactmodn1(uint64_t k, uint64_t n) {
-  if (k >= n) return 0;
-  if (n >> 32) {
-    uint64_t f = 1;
-    for (uint64_t i=2; i<=k; i++) f = ((U128)f*i) % n;
-    return f;
-  } 
-  uint32_t f = 1;
-  for (uint32_t i=2; i<=k; i++) f = ((uint64_t)f*i) % n;
-  return f;
 }
 
 uint64_t getkfactmodn6(uint32_t k, uint64_t n, uint32_t numprimes, uint32_t *primes) {
@@ -91,6 +80,20 @@ uint64_t getkfactmodn6(uint32_t k, uint64_t n, uint32_t numprimes, uint32_t *pri
   }
   return res;
 }
+#endif
+
+uint64_t getkfactmodn1(uint64_t k, uint64_t n) {
+  if (k >= n) return 0;
+  if (n >> 32) {
+    uint64_t f = 1;
+    for (uint64_t i=2; i<=k; i++) f = ((U128)f*i) % n;
+    return f;
+  } 
+  uint32_t f = 1;
+  for (uint32_t i=2; i<=k; i++) f = ((uint64_t)f*i) % n;
+  return f;
+}
+
 
 //===============================================================================================================================
 // kfactorialmodn.c: END
